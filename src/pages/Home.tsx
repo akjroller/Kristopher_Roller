1317d312fbf72848e6f710da59f882af25c2b9dd
Kristopher Roller <akjroller@gmail.com>
2025-10-26 22:22:51 -0400

    Add leadership values section to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,6 +4,7 @@ import HomeHighlights from '../components/sections/HomeHighlights';
 import ImpactTeaser from '../components/sections/ImpactTeaser';
 import ReadyCTA from '../components/sections/ReadyCTA';
 import SkillShowcase from '../components/sections/SkillShowcase';
+import LeadershipValues from '../components/sections/LeadershipValues';
 
 const Home = () => (
   <div className="space-y-12">
@@ -13,6 +14,7 @@ const Home = () => (
     <HeroBanner />
     <HomeHighlights />
     <SkillShowcase />
+    <LeadershipValues />
     <ImpactTeaser />
     <ReadyCTA />
   </div>
